66c2b29cac8e26abbdaeb29b688caaaf9ac7080d
Code of K <korjon0000@gmail.com>
2023-10-10 16:54:23 -0400

    created all necessary entities

--- online_store/src/main/java/soen/online_store/java/Customer.java
+++ online_store/src/main/java/soen/online_store/java/Customer.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package soen.online_store.java;
 
 /**
  *
  * @author Kojo
  */
-public class Customer {
+public class Customer extends User{
+    
+    private String customerId;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+    public Customer(String customerId, String address, String phoneNumber, String userID, String username, String password, String role) {
+        super(userID, username, password, role);
+        this.customerId = customerId;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
--- online_store/src/main/java/soen/online_store/java/Employee.java
+++ online_store/src/main/java/soen/online_store/java/Employee.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package soen.online_store.java;
 
 /**
  *
  * @author Kojo
  */
-public class Employee {
+public class Employee extends User{
+    
+    private String employeeId;
+    private String startDate;
+    private String endDate;
+    private String title;
+
+    public Employee(String employeeId, String startDate, String endDate, String title, String userID, String username, String password, String role) {
+        super(userID, username, password, role);
+        this.employeeId = employeeId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
     
 }
