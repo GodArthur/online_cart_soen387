326638a0f5d8728cb453855629ee2c695a1e1549
unknown <korjon0000@gmail.com>
2023-11-05 23:50:35 -0500

    created a class to perform crud operations on database and implemented createProducts method. Created the config.properties file for the db configurations, wrote the database script. Created a context listener to create a servlet context based on the config.properties file

---
+++ online_store/src/main/java/soen/online_store/java/listeners/DBContextListener.java
@@ -0,0 +1,38 @@
+package soen.online_store.java.listeners;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.ServletContext;
+import java.util.Properties;
+import java.io.InputStream;
+/**
+ *
+ * @author Korjon Chang-Jones
+ */
+@WebListener
+public class DBContextListener implements ServletContextListener{
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Properties configProps = new Properties();
+        String configFile = "/WEB-INF/config.properties"; // The path to the config file
+
+        try (InputStream inputStream = sce.getServletContext().getResourceAsStream(configFile)) {
+            if (inputStream == null) {
+                throw new IllegalStateException("Configuration file '" + configFile + "' not found in the application.");
+            }
+            configProps.load(inputStream);
+            // Store the configuration to be accessible in the servlet context
+            sce.getServletContext().setAttribute("dbConfig", configProps);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database configuration", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Perform cleanup if necessary
+    }
+    
+}
